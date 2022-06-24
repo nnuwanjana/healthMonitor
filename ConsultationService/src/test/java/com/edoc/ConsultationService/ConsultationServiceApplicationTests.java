0b14e2ae2dd46c4cc845db124c6fdffbddf7fadc
Nimesh Nuwanjana <nimeshnuwanjanamd@gmail.com>
2022-06-24 16:22:29 +0530

    TASK-001 : Initial Commit

---
+++ ConsultationService/src/test/java/com/edoc/ConsultationService/ConsultationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edoc.ConsultationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
